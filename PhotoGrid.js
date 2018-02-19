012cfb6d4c16383d8ee7748a4cd0afc152ea5abe
Daniel Hollcraft <danielbh@users.noreply.github.com>
2018-02-18 20:56:35 -0500

    Update PhotoGrid.js

--- PhotoGrid.js
+++ PhotoGrid.js
@@ -1,73 +1,96 @@
-'use strict';
-
-import React, { Component } from 'react';
+/**
+ * Fork of an OSS MIT licensed npm module.
+ * Infinite scroll was added, code was cleaned up, and onTouch callback added.
+ * LICENSE and COPYRIGHT: http://bit.ly/2Hp0Lxn
+ */
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import {
-  Dimensions,
   ListView,
   StyleSheet,
   View,
-} from 'react-native';
+} from 'react-native'
 
-let styles = StyleSheet.create({
+import InfiniteScrollView from 'react-native-infinite-scroll-view'
+
+const styles = StyleSheet.create({
   row: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
   }
-});
+})
 
-class PhotoGrid extends React.Component {
-
+class ImageGrid extends Component {
   constructor() {
-    super();
+    super()
 
     this.state = {
       data: new ListView.DataSource({
-        rowHasChanged: (r1, r2) => { r1 !== r2; }
+        rowHasChanged: (r1, r2) => r1 !== r2
       }),
     }
+
+    this.renderRow = this.renderRow.bind(this)
   }
 
   buildRows(items, itemsPerRow = 3) {
     return items.reduce((rows, item, idx) => {
       // If a full row is filled create a new row array
-      if(idx % itemsPerRow === 0 && idx > 0) rows.push([]);
-      rows[rows.length-1].push(item);
-      return rows;
-    }, [[]]);
-  }
-
-  render() {
-    let rows = this.buildRows(this.props.data, this.props.itemsPerRow);
-
-    return (
-      <ListView
-        { ...this.props }
-        dataSource = { this.state.data.cloneWithRows(rows) }
-        renderRow = { this.renderRow.bind(this) }
-        style = {{ flex: 1 }} />
-    );
+      if (idx % itemsPerRow === 0 && idx > 0) rows.push([])
+      rows[rows.length - 1].push(item)
+      return rows
+    }, [[]])
   }
 
   renderRow(items) {
     // Calculate the width of a single item based on the device width
     // and the desired margins between individual items
-    let deviceWidth = Dimensions.get('window').width;
-    let itemsPerRow = this.props.itemsPerRow;
-    let margin = this.props.itemMargin || 1;
-
-    let totalMargin = margin * (itemsPerRow - 1);
-    let itemWidth = Math.floor( (deviceWidth - totalMargin) / itemsPerRow );
-    let adjustedMargin = ( deviceWidth - (itemsPerRow*itemWidth) ) / (itemsPerRow - 1);
+    const { itemsPerRow, itemMargin, deviceWidth } = this.props
+    const totalMargin = itemMargin * (itemsPerRow - 1)
+    const itemWidth = Math.floor((deviceWidth - totalMargin) / itemsPerRow)
+    const adjustedMargin = (deviceWidth - (itemsPerRow * itemWidth)) / (itemsPerRow - 1)
 
     return (
-      <View style = {[ styles.row, { marginBottom: adjustedMargin } ]}>
-        { items.map(item => this.props.renderItem(item, itemWidth)) }
-        { itemsPerRow - items.length > 0 && <View style={{width: itemWidth * (itemsPerRow - items.length)}} />}
+      <View style={[styles.row, { marginBottom: adjustedMargin }]}>
+        {items.map(item =>
+          this.props.renderItem(item, itemWidth, this.props.onItemTouch))}
+        {itemsPerRow - items.length > 0 &&
+          <View style={{ width: itemWidth * (itemsPerRow - items.length) }} />
+        }
       </View>
-    );
+    )
   }
 
+  render() {
+    const rows = this.buildRows(this.props.data, this.props.itemsPerRow)
+
+    return (
+      <ListView
+        renderScrollComponent={props => <InfiniteScrollView {...props} />}
+        {...this.props}
+        dataSource={this.state.data.cloneWithRows(rows)}
+        renderRow={this.renderRow}
+        style={{ flex: 1 }}
+        canLoadMore
+        onLoadMoreAsync={this.props.loadMoreContentAsync}
+      />
+    )
+  }
 }
 
-export default PhotoGrid;
+ImageGrid.propTypes = {
+  data: PropTypes.array.isRequired,
+  renderItem: PropTypes.func.isRequired,
+  onItemTouch: PropTypes.func.isRequired,
+  loadMoreContentAsync: PropTypes.func.isRequired,
+  itemsPerRow: PropTypes.number.isRequired,
+  itemMargin: PropTypes.number,
+  deviceWidth: PropTypes.number.isRequired
+}
+
+ImageGrid.defaultProps = {
+  itemMargin: 0
+}
+
+export default ImageGrid
